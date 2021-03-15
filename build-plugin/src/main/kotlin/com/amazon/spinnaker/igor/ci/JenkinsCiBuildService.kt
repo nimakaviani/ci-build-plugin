5ae5288f91f2c0a3bc0763e8bcd5f3b1a1c13c9a
Nima Kaviani <nkaviani@amazon.com>
2021-03-14 21:09:39 -0700

    add tests to the plugin
    - testing getBuilds
    - testinggetBuildOutput

--- build-plugin/src/main/kotlin/com/amazon/spinnaker/igor/ci/JenkinsCiBuildService.kt
+++ build-plugin/src/main/kotlin/com/amazon/spinnaker/igor/ci/JenkinsCiBuildService.kt
@@ -17,57 +17,53 @@
 
 package com.amazon.spinnaker.igor.ci;
 
-import com.netflix.spinnaker.igor.build.model.GenericArtifact;
-import com.netflix.spinnaker.igor.build.model.GenericBuild;
-import com.netflix.spinnaker.igor.build.model.GenericGitRevision;
-import com.netflix.spinnaker.igor.ci.CiBuildService;
+import com.netflix.spinnaker.igor.build.model.GenericArtifact
+import com.netflix.spinnaker.igor.build.model.GenericBuild
+import com.netflix.spinnaker.igor.build.model.GenericGitRevision
+import com.netflix.spinnaker.igor.ci.CiBuildService
 import com.netflix.spinnaker.igor.config.JenkinsConfig
 import com.netflix.spinnaker.igor.config.JenkinsProperties
 import com.netflix.spinnaker.igor.config.client.JenkinsOkHttpClientProvider
 import com.netflix.spinnaker.igor.config.client.JenkinsRetrofitRequestInterceptorProvider
-import com.netflix.spinnaker.igor.docker.model.DockerRegistryAccounts;
-import com.netflix.spinnaker.igor.docker.service.TaggedImage;
-import com.netflix.spinnaker.igor.jenkins.client.JenkinsClient;
-import com.netflix.spinnaker.igor.jenkins.client.model.Build;
-import com.netflix.spinnaker.security.AuthenticatedRequest;
-import com.netflix.spinnaker.igor.jenkins.client.model.ScmDetails;
-import com.netflix.spinnaker.igor.scm.AbstractScmMaster;
+import com.netflix.spinnaker.igor.docker.model.DockerRegistryAccounts
+import com.netflix.spinnaker.igor.docker.service.TaggedImage
+import com.netflix.spinnaker.igor.jenkins.client.JenkinsClient
+import com.netflix.spinnaker.igor.jenkins.client.model.Build
+import com.netflix.spinnaker.igor.jenkins.client.model.ScmDetails
+import com.netflix.spinnaker.igor.scm.AbstractScmMaster
 import com.netflix.spinnaker.igor.scm.github.client.GitHubMaster
-import com.netflix.spinnaker.igor.scm.github.client.model.Commit;
-import com.netflix.spinnaker.kork.core.RetrySupport;
-import com.netflix.spectator.api.Registry
-import io.github.resilience4j.circuitbreaker.CircuitBreaker;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import com.netflix.spinnaker.igor.scm.github.client.model.Commit
+import com.netflix.spinnaker.kork.core.RetrySupport
+import com.netflix.spinnaker.security.AuthenticatedRequest
+import io.github.resilience4j.circuitbreaker.CircuitBreaker
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry
 import mu.KotlinLogging
-import org.apache.commons.io.IOUtils;
-import org.springframework.web.util.UriUtils;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
-import retrofit.RestAdapter
-
+import org.apache.commons.io.IOUtils
+import org.springframework.web.util.UriUtils
+import retrofit.RetrofitError
+import retrofit.client.Response
 import java.nio.charset.StandardCharsets
 import java.time.Duration
 import java.util.function.Predicate
-import java.util.regex.Pattern;
+import java.util.regex.Pattern
 import javax.net.ssl.*
 
 class JenkinsCiBuildService(
-        private val jenkinsProperties: JenkinsProperties,
-        private val dockerRegistryAccounts:DockerRegistryAccounts,
-        private val circuitBreakerRegistry:CircuitBreakerRegistry,
-        private val scmMasters:List<AbstractScmMaster>,
-        private val jenkinsOkHttpClientProvider:JenkinsOkHttpClientProvider,
-        private val jenkinsRetrofitRequestInterceptorProvider:JenkinsRetrofitRequestInterceptorProvider,
-        private val registry: Registry,
+    private val jenkinsProperties: JenkinsProperties,
+    private val dockerRegistryAccounts: DockerRegistryAccounts,
+    private val circuitBreakerRegistry: CircuitBreakerRegistry,
+    private val scmMasters: List<AbstractScmMaster>,
+    private val jenkinsOkHttpClientProvider: JenkinsOkHttpClientProvider,
+    private val jenkinsRetrofitRequestInterceptorProvider: JenkinsRetrofitRequestInterceptorProvider,
 ) : CiBuildService {
 
     private val logger = KotlinLogging.logger {}
     private var retrySupport : RetrySupport
     private var circuitBreaker : CircuitBreaker
-    private lateinit var jenkinsClient : JenkinsClient
-    private lateinit var scmMaster : GitHubMaster
-    private lateinit var jenkinsHostId : String
+    lateinit var jenkinsClient : JenkinsClient
+    lateinit var scmMaster : GitHubMaster
+    lateinit var jenkinsHostId : String
 
     init {
         configure()
@@ -76,20 +72,23 @@ class JenkinsCiBuildService(
             circuitBreakerRegistry.circuitBreaker(
                 "jenkins-" + jenkinsHostId,
                 CircuitBreakerConfig.custom()
-                    .ignoreException (Predicate<Throwable> { re ->
+                    .ignoreException(Predicate<Throwable> { re ->
                         if (re is RetrofitError) {
-                            return@Predicate re.getKind() == RetrofitError.Kind.HTTP && re.getResponse().getStatus() == 404
+                            return@Predicate re.getKind() == RetrofitError.Kind.HTTP && re.getResponse()
+                                .getStatus() == 404
                         }
                         return@Predicate false
-                    }).build())
+                    }).build()
+            )
     }
 
   override fun getBuilds(
-    projectKey: String,
-    repoSlug: String,
-    buildNumber: String?,
-    commitId: String?,
-    completionStatus: String?) : List<GenericBuild>{
+      projectKey: String,
+      repoSlug: String,
+      buildNumber: String?,
+      commitId: String?,
+      completionStatus: String?
+  ) : List<GenericBuild>{
     // projectKey is supposed to match the Jenkins job name
     var jobName = projectKey
     val imageArtifacts = getImages()
@@ -104,22 +103,32 @@ class JenkinsCiBuildService(
     }
 
     val genericBuilds = mutableListOf<GenericBuild>();
-    callBuilds(projectKey).stream().forEach{
-        b -> genericBuilds.add(getGenericBuildWithGitDetails(jobName, b.getNumber(), imageArtifacts))
+    callBuilds(projectKey).stream().forEach{ b -> genericBuilds.add(
+        getGenericBuildWithGitDetails(
+            jobName,
+            b.getNumber(),
+            imageArtifacts
+        )
+    )
     };
     return genericBuilds;
   }
 
-  override fun getBuildOutput(buildId:String) : Map<String, Any> {
+  override fun getBuildOutput(buildId: String) : Map<String, Any> {
     val p = Pattern.compile("(.+)-(.+)");
     val m = p.matcher(buildId);
     if (!m.find()) return emptyMap()
 
-    callBuildOutput(m.group(1), m.group(2))?.let {
-        val body = IOUtils.toString(it.getBody().`in`(), StandardCharsets.UTF_8)
-        return mapOf("log" to body)
+    try {
+        callBuildOutput(m.group(1), m.group(2))?.let {
+            val body = IOUtils.toString(it.getBody().`in`(), StandardCharsets.UTF_8)
+            return mapOf("log" to body)
+        }
+    }catch(e: Exception) {
+        logger.error(e.message)
     }
-    return emptyMap()
+
+    return mapOf("log" to "")
   }
 
    fun getGenericBuildWithGitDetails(jobName: String, buildNumber: Int, images: List<TaggedImage>) : GenericBuild {
@@ -132,10 +141,18 @@ class JenkinsCiBuildService(
     revs.forEach { grv ->
       val m = p.matcher(grv.remoteUrl);
       if (m.find()) {
-        val commit = this.scmMaster.getCommitDetails(m.group(1), m.group(2), grv.getSha1()) as Commit;
+        val commit = try {
+            this.scmMaster.getCommitDetails(m.group(1), m.group(2), grv.getSha1()) as Commit
+        } catch (e: Exception) {
+            return@forEach
+        }
         properties.put("projectKey", m.group(1))
         properties.put("repoSlug", m.group(2))
-        grv.withAuthorInfo(commit.getAuthor().getName(), commit.getMessage()).withTimestamp(commit.getAuthor().getDate()).withLink(commit.html_url)
+        grv.withAuthorInfo(commit.getAuthor().getName(), commit.getMessage()).withTimestamp(
+            commit.getAuthor().getDate()
+        ).withCompareUrl(commit.html_url)
+
+        // pull artifact details based on the buildNumber and commit sha1 in its metadata
         getArtifact(images, jobName, buildNumber, commit.getSha())?.let {
             artifacts.add(it)
         }
@@ -160,20 +177,22 @@ class JenkinsCiBuildService(
   }
 
   private fun getGitDetails(jobName: String, buildNumber: Int) : ScmDetails? {
-    return retrySupport.retry({
-        try {
-          return@retry jenkinsClient.getGitDetails(encode(jobName), buildNumber)
-        } catch (e: RetrofitError) {
-          // assuming that a conversion error is unlikely to succeed on retry
-          if (e.getKind() == RetrofitError.Kind.CONVERSION) {
-            return@retry null;
-          } else {
-            throw e;
-          }
-        }
-      },
-      10,
-      Duration.ofMillis(1000), false);
+    return retrySupport.retry(
+        {
+            try {
+                return@retry jenkinsClient.getGitDetails(encode(jobName), buildNumber)
+            } catch (e: RetrofitError) {
+                // assuming that a conversion error is unlikely to succeed on retry
+                if (e.getKind() == RetrofitError.Kind.CONVERSION) {
+                    return@retry null;
+                } else {
+                    throw e;
+                }
+            }
+        },
+        10,
+        Duration.ofMillis(1000), false
+    );
   }
 
   private fun getImages() : List<TaggedImage> {
@@ -181,16 +200,22 @@ class JenkinsCiBuildService(
     this.dockerRegistryAccounts.getAccounts().forEach { account ->
       val accountName : String = account.get("name") as String
       images.addAll(AuthenticatedRequest.allowAnonymous {
-        dockerRegistryAccounts.service.getImagesByAccount(accountName, true)
+          dockerRegistryAccounts.service.getImagesByAccount(accountName, true)
       })
     }
     return images
   }
 
   private fun getArtifact(images: List<TaggedImage>, jobName: String, buildNumber: Int, commitId: String): GenericArtifact? {
-    images.forEach {im ->
+    images.forEach { im ->
       if (im.buildNumber == buildNumber.toString() && im.commitId == commitId) {
-        return@getArtifact (GenericArtifact("docker", im.repository, im.tag, im.digest)).withUrl(im.registry)
+        return@getArtifact GenericArtifact("docker", im.repository, im.tag, im.digest).withUrl(
+            String.format(
+                "https://%s/v2/%s/blobs/%s",
+                im.registry,
+                im.repository,
+                im.digest
+            ))
       }
     }
 
@@ -212,13 +237,12 @@ class JenkinsCiBuildService(
       }
 
       jenkinsProperties.masters.forEach { host ->
+          jenkinsHostId = host.name
           if (host.ciEnabled) {
-              jenkinsHostId = host.name
               jenkinsClient = JenkinsConfig.jenkinsClient(
-                      host,
-                      jenkinsOkHttpClientProvider,
-                      jenkinsRetrofitRequestInterceptorProvider,
-                      registry,
+                  host,
+                  jenkinsOkHttpClientProvider,
+                  jenkinsRetrofitRequestInterceptorProvider,
               )
               return@forEach
           }
---
+++ build-plugin/src/test/kotlin/com/amazon/spinnaker/igor/ci/JenkinsCiBuildServiceTest.kt
@@ -0,0 +1,250 @@
+package com.amazon.spinnaker.igor.ci
+
+import com.netflix.spinnaker.igor.build.model.Result
+import com.netflix.spinnaker.igor.config.JenkinsProperties
+import com.netflix.spinnaker.igor.config.client.DefaultJenkinsOkHttpClientProvider
+import com.netflix.spinnaker.igor.config.client.DefaultJenkinsRetrofitRequestInterceptorProvider
+import com.netflix.spinnaker.igor.docker.model.DockerRegistryAccounts
+import com.netflix.spinnaker.igor.docker.service.ClouddriverService
+import com.netflix.spinnaker.igor.docker.service.TaggedImage
+import com.netflix.spinnaker.igor.jenkins.client.JenkinsClient
+import com.netflix.spinnaker.igor.jenkins.client.model.*
+import com.netflix.spinnaker.igor.scm.github.client.GitHubMaster
+import com.netflix.spinnaker.igor.scm.github.client.model.Author
+import com.netflix.spinnaker.igor.scm.github.client.model.Commit
+import com.netflix.spinnaker.igor.scm.github.client.model.CommitInfo
+import dev.minutest.junit.JUnit5Minutests
+import dev.minutest.rootContext
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry
+import io.mockk.coEvery
+import io.mockk.mockk
+import okhttp3.OkHttpClient
+import retrofit.client.Response
+import retrofit.mime.TypedInput
+import strikt.api.expectCatching
+import strikt.api.expectThat
+import strikt.assertions.*
+
+@Suppress("UNCHECKED_CAST")
+internal class JenkinsCiBuildServiceTest : JUnit5Minutests {
+
+    private val props = JenkinsProperties()
+    private val okHttpClient : OkHttpClient = mockk()
+    private val mockScmMaster : GitHubMaster = mockk()
+    private val defaultProvider = DefaultJenkinsOkHttpClientProvider(okHttpClient)
+    private val defaultInterceptor = DefaultJenkinsRetrofitRequestInterceptorProvider()
+    private val dockerAccounts = DockerRegistryAccounts()
+    private val jenkinsClient = mockk<JenkinsClient>()
+    private val clouddriverService = mockk<ClouddriverService>()
+    private lateinit var buildService : JenkinsCiBuildService
+
+    fun taggedImage(repo: String, buildNumber: String, commitSha: String, digest: String) : TaggedImage {
+        val img = TaggedImage()
+        img.account = "some-account"
+        img.branch = "main"
+        img.buildNumber = buildNumber
+        img.commitId = commitSha
+        img.digest = digest
+        img.repository = repo
+        return img
+    }
+
+    fun getBranch(name: String, sha: String): Branch {
+        val br = Branch()
+        br.name = name
+        br.sha1 = sha
+        return br
+    }
+
+    fun getRevision(b: Branch) : Revision{
+        val r = Revision()
+        r.branch = listOf(b)
+        return r
+    }
+
+    fun getBuild(rev: Revision) : ScmBuild {
+        val b = ScmBuild()
+        b.revision = rev
+        return b
+    }
+
+    fun getAction(url: String, b: ScmBuild) : Action {
+        val a = Action()
+        a.remoteUrl = url
+        a.build = b
+        return a
+    }
+
+    fun getScmDetails(a: Action?) : ScmDetails {
+        val details = ScmDetails()
+        details.actions = arrayListOf(a)
+        return details
+    }
+
+    fun getCommitDetails() : Commit {
+        val c = Commit()
+        c.url = "some-url"
+        c.sha = "some-sha"
+        c.message = "some-message"
+        c.html_url = "some-html-url"
+
+        val a = Author()
+        a.name = "some-name"
+        a.email = "alias@example.com"
+
+        val ci = CommitInfo()
+        ci.message = "some-commit-msg"
+        ci.author = a
+
+        c.author = a
+        return c
+    }
+
+    fun getBuildsResult() : Build {
+        val b = Build()
+        b.building = false
+        b.number = 4
+        b.result = "SUCCESS"
+        b.timestamp = "1613503078265"
+        b.duration = 37527
+        b.url = "some-url"
+        return b
+    }
+
+    fun getBuildList() : BuildsList {
+        val bl = BuildsList()
+        bl.list = listOf(getBuildsResult())
+        return bl
+    }
+
+    fun getJenkinsHost() : JenkinsProperties.JenkinsHost {
+        val h = JenkinsProperties.JenkinsHost()
+        h.name    = "some-host"
+        h.address = "some-address"
+        return h
+    }
+
+    fun getResponse() : Response {
+        val body = mockk<TypedInput>()
+        coEvery { body.`in`() } returns "yolo".toByteArray(Charsets.UTF_8).inputStream()
+        return Response("som-url", 200, "good", mutableListOf(), body)
+    }
+
+    fun tests() = rootContext {
+        before {
+            dockerAccounts.service = clouddriverService
+            dockerAccounts.accounts.add(mapOf("name" to "something"))
+            props.masters = listOf(getJenkinsHost())
+            buildService = JenkinsCiBuildService(
+                    props,
+                    dockerAccounts,
+                    CircuitBreakerRegistry.ofDefaults(),
+                    listOf(mockScmMaster),
+                    defaultProvider,
+                    defaultInterceptor
+            )
+            buildService.jenkinsHostId = "some-host"
+            buildService.jenkinsClient = jenkinsClient
+        }
+
+        context("for when config is provided") {
+            before {
+                val a = getAction("github.com/test/repo.git", getBuild(getRevision(getBranch("some-branch", "some-sha1"))))
+                coEvery { clouddriverService.getImagesByAccount("something", true) } returns listOf(
+                    taggedImage("example/img-fake", "1", "some-commit", "digest1"),
+                    taggedImage("example/img-real","4", "some-sha", "digest2")
+                )
+                coEvery { jenkinsClient.getGitDetails(any(),any()) } returns getScmDetails(a)
+                coEvery { jenkinsClient.getBuilds(any())} returns getBuildList()
+                coEvery { jenkinsClient.getBuild("some-project", 4)} returns getBuildsResult()
+                coEvery { mockScmMaster.getCommitDetails("test", "repo", any()) } returns getCommitDetails()
+            }
+
+            test("fetching builds succeeds with proper data") {
+                val b  = buildService.getBuilds("some-project", "some-slug", null, null, null)
+                expectThat(b.size).equals(1)
+
+                val buildData = b.first()
+                expectThat(buildData) {
+                    get { id }.isEqualTo("some-project-4")
+                    get { result }.isEqualTo(Result.SUCCESS)
+                    get { genericGitRevisions.size }.isEqualTo(1)
+                    get { artifacts }.get { size }.isEqualTo(1)
+                    get { artifacts }.get { first() }.get { name }.isEqualTo("example/img-real")
+                }
+
+                 expectThat(buildData.genericGitRevisions.first()) {
+                    get { name }.isEqualTo("some-branch")
+                    get { message }.isEqualTo("some-message")
+                    get { sha1 }.isEqualTo("some-sha1")
+                    get { branch }.isEqualTo("some-branch")
+                    get { compareUrl }.isEqualTo("some-html-url")
+                     get { committer }.isEqualTo("some-name")
+                 }
+            }
+
+            context("when no github action is available for build") {
+                before {
+                    coEvery { jenkinsClient.getGitDetails(any(), any()) } returns getScmDetails(null)
+                }
+
+                test("build data exists but with no git info") {
+                    val b  = buildService.getBuilds("some-project", "some-slug", null, null, null)
+                    expectThat(b.size).equals(1)
+                    expectThat(b.first().genericGitRevisions).isEmpty()
+                }
+            }
+
+            context("when no commit details are found") {
+                before {
+                    coEvery { mockScmMaster.getCommitDetails("test", "repo", any()) } throws Exception("not found")
+                }
+
+                test("committer details and artifact info are missing") {
+                    val b  = buildService.getBuilds("some-project", "some-slug", null, null, null)
+                    expectThat(b.size).equals(1)
+                    expectThat(b.first().genericGitRevisions.size).isEqualTo(1)
+                    expectThat(b.first().genericGitRevisions.first()) { get { committer }.isNull() }
+                    expectThat(b.first().artifacts).isEmpty()
+                }
+            }
+
+            context("docker throws error") {
+                before {
+                    coEvery { clouddriverService.getImagesByAccount(any(), any()) } throws Exception("not found")
+                }
+
+                test("committer details and artifact info are missing") {
+                    expectCatching {
+                        buildService.getBuilds("some-project", "some-slug", null, null, null)
+                    }.isFailure()
+                }
+            }
+        }
+
+        context("when fetching build output") {
+            context("when data is valid") {
+                before {
+                    coEvery { jenkinsClient.getBuildOutput("build", "4")} returns getResponse()
+                }
+
+                test("it returns output") {
+                    val output = buildService.getBuildOutput("build-4")
+                    expectThat(output).isEqualTo(mapOf("log" to "yolo"))
+                }
+            }
+
+            context("when build output throws exception") {
+                before {
+                    coEvery { jenkinsClient.getBuildOutput(any(), any())} throws Exception("not found")
+                }
+
+                test("it returns empty output") {
+                    expectCatching { buildService.getBuildOutput("build-4") }
+                        .isSuccess()
+                        .isEqualTo(mapOf("log" to ""))
+                }
+            }
+        }
+    }
+}
